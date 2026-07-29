33a2faca1a4fdd3d5b47cb9606278b7300262e21
badbox29 <96398618+badbox29@users.noreply.github.com>
2026-07-29 13:16:59 -0400

    Update sheet_template.js

--- js/sheet_template.js
+++ js/sheet_template.js
@@ -127,11 +127,11 @@ const SHEET_HTML = `
               </div>
               <div class="col" style="flex:1">
                 <label>XP</label>
-                <input data-field="mc_xpi" type="text" readonly>
+                <input data-field="mc_xp1" type="text" readonly>
               </div>
               <div class="col" style="flex:2">
                 <label>XP for Next Level</label>
-                <input data-field="mc_xp_nexti" type="text" readonly>
+               <input data-field="mc_xp_next1" type="text" readonly>
               </div>
             </div>
             <div class="row" style="margin-top:4px;padding:8px;background:rgba(0,0,0,0.1);border-radius:4px;">
@@ -150,16 +150,16 @@ const SHEET_HTML = `
                 </select>
               </div>
               <div class="col" style="flex:1">
+                <label>Level</label>
+                <input data-field="mc_level2" type="number" min="1" value="1">
+              </div>
+              <div class="col" style="flex:1">
                 <label>XP</label>
                 <input data-field="mc_xp2" type="text" readonly>
               </div>
               <div class="col" style="flex:2">
                 <label>XP for Next Level</label>
                 <input data-field="mc_xp_next2" type="text" readonly>
-              </div>
-              <div class="col" style="flex:1">
-                <label>XP</label>
-                <input data-field="mc_xp2" type="text" readonly>
               </div>
             </div>
             <div class="row" style="margin-top:4px;padding:8px;background:rgba(0,0,0,0.1);border-radius:4px;">
