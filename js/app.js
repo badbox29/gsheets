58440e62b2a56ce8d8b6aeb8d02d72b670da7818
badbox29 <96398618+badbox29@users.noreply.github.com>
2026-08-27 15:50:33 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7672,6 +7672,7 @@ function loadSheet(root, data){
   if (typeof renderClassStatus === 'function') renderClassStatus(root);
   if (typeof renderSpecialtyPriestBanners === 'function') renderSpecialtyPriestBanners(root);
   if (typeof renderSpecialtyPriestChecks === 'function') renderSpecialtyPriestChecks(root);
+  if (typeof renderKitRequirements === 'function') renderKitRequirements(root);
   if (typeof populatePriesthoodTemplates === 'function') populatePriesthoodTemplates(root);
   if (typeof renderSpecialtyPriest === 'function') renderSpecialtyPriest(root);
   renderCoinWeight(root);
@@ -8892,6 +8893,7 @@ function bindSheet(root, tab){
   if (typeof renderClassStatus === 'function') renderClassStatus(root);
   if (typeof renderSpecialtyPriestBanners === 'function') renderSpecialtyPriestBanners(root);
   if (typeof renderSpecialtyPriestChecks === 'function') renderSpecialtyPriestChecks(root);
+  if (typeof renderKitRequirements === 'function') renderKitRequirements(root);
   if (typeof populatePriesthoodTemplates === 'function') populatePriesthoodTemplates(root);
   if (typeof renderSpecialtyPriest === 'function') renderSpecialtyPriest(root);
   renderCoinWeight(root);
@@ -16181,6 +16183,7 @@ function recalculateAll(root) {
   if (typeof renderClassStatus === 'function') renderClassStatus(root);
   if (typeof renderSpecialtyPriestBanners === 'function') renderSpecialtyPriestBanners(root);
   if (typeof renderSpecialtyPriestChecks === 'function') renderSpecialtyPriestChecks(root);
+  if (typeof renderKitRequirements === 'function') renderKitRequirements(root);
   if (typeof populatePriesthoodTemplates === 'function') populatePriesthoodTemplates(root);
   if (typeof renderSpecialtyPriest === 'function') renderSpecialtyPriest(root);
   if (typeof renderPrimeRequisiteBonus === 'function') renderPrimeRequisiteBonus(root);
